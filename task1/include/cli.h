a0844e2e9bf3c796b035e435ab1523418d751db3
xsestech <xsestech@gmail.com>
2024-09-14 10:06:47 +0300

    added clang format

---
+++ task1/include/cli.h
@@ -0,0 +1,12 @@
+//
+// Created by Alex on 14.09.2024.
+//
+
+#ifndef CLI_H
+#define CLI_H
+
+typedef cli_arg_config_t cli_arg_config_t;
+
+typedef struct cli_config_t cli_config_t;
+
+#endif //CLI_H
--- task1/src/cli.c
+++ task1/src/cli.c
@@ -1,3 +1,8 @@
 //
 // Created by Alex on 14.09.2024.
 //
+
+struct cli_config_t {
+  char argument_name;
+  
+};
